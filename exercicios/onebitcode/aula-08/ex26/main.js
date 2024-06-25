e8018ad5b40ffb48f9655a5373fdcd4c278c2d28
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2024-06-25 17:47:04 -0300

    Atualizando

--- exercicios/onebitcode/aula-08/ex26/main.js
+++ exercicios/onebitcode/aula-08/ex26/main.js
@@ -10,14 +10,75 @@ async function exibirUsers() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users")
   const dados = await response.json()
 
-  console.log(response)
+  console.log(dados)
   
   postsList.innerHTML = ""
   
   dados.forEach(function(user) {
     const listElement = document.createElement("li")
 
-    listElement.innerHTML = `${user.id}`
+    listElement.innerHTML = 
+    `<article>
+      <p>
+        Id: ${user.id}
+      </p>
+
+      <p>
+        Nome: ${user.name}
+      </p>
+
+      <p>
+        Nome de Usuário: ${user.username}
+      </p>
+
+      <p>
+        Email: ${user.email}
+      </p>
+
+      <p>
+        Telefone: ${user.phone}
+      </p>
+
+      <p>
+        Site: ${user.website}
+      </p>
+
+      <p>
+        Endereço: 
+
+        <ul>
+
+          <li style="list-style: disc;">
+
+            <p>
+              <strong>Cidade:</strong> ${user.address.city}
+            </p>
+
+            <p>
+              Rua: ${user.address.street}
+            </p>
+
+            <p>
+              Suíte: ${user.address.suite}
+            </p>
+
+            <p>
+              Código Postal: ${user.address.zipcode}
+            </p>
+
+            <p>
+              Geográfico: Lat (${user.address.geo.lat}) / Lng (${user.address.geo.lng})
+            </p>
+
+          </li>
+
+        </ul>
+      </p>
+
+      <p>
+
+      </p>
+      </article>`
 
     postsList.append(listElement)
 
